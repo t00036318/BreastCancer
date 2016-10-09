c75199bf2c080098250811f7f1710b5e9f747fef
Nicolas Agudelo <nicoalbert95@hotmail.com>
2016-10-09 17:47:34 -0500

    mitad de gradiente descendiente

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+import copy as c
 def featureNormalize(X):
 
     [m,n] = X.shape
@@ -17,6 +18,24 @@ def featureNormalize(X):
             X[k,j]=(X[k,j]-mean[j])/std[j]
     return X
 
+def gradientDescent (X, y, theta, alpha, num_iters):
+    J_histor = []
+    error = 0.0
+    [m,n] = X.shape         # m = numero de ejemplos de entrenamiento
+    i=0
+    theta_temp = []
+    j=0
+    for i in range (num_iters):
+        for j in range (116):
+            h = np.dot(X[j,:],theta)
+            print (h)
+            error = h-y[j]
+        error/=116
+
+
+    return [theta,J_histor]
+
+
 def main():
     f = open('r_wpbc.data', 'r')
 
@@ -32,13 +51,6 @@ def main():
     x = np.matrix(data)         #Matriz x (entradas) es 194*33
 
 
-    y = []                      #Vector columna con salidas (Time)
-    for i in range(194):
-        y.append(x[i,32])
-
-    x = np.delete(x, 32, 1)     #Matriz x (entradas) es 194*32  (vector y eliminado de x)
-
-
     t = random.sample(range(194),116)
     training_ds = x[t,:]        #Matriz con datos de entrenamiento (60% de los datos iniciales)
 
@@ -48,7 +60,28 @@ def main():
             t2.append(element)
 
     tests_ds = x[t2,:]          #Matriz con datos de prueba (40% de los datos iniciales)
+    y_training = []
+    y_test =[]
+    for i in range (116):
+        y_training.append(training_ds[i,32])
+    for i in range (78):
+        y_test.append(tests_ds[i,32])
+    x = np.delete(x,32,1)       #Matriz x (entradas) es 194*32  (vector y eliminado de x)
+    training_ds = np.delete(training_ds,32,1)
+    tests_ds = np.delete(tests_ds,32,1)
+
+
     training_ds=featureNormalize(training_ds) #Normalizar datos del conjunto de entrenamiento
-    
+    training_ds=np.concatenate((np.ones((116,1)),training_ds),axis=1)
+    tests_ds = np.concatenate((np.ones((78,1)),tests_ds),axis=1)
+    theta = np.zeros((33,1))
+    alpha = 0.09
+    iterat = 400
+    temp = gradientDescent(training_ds,y_training,theta,alpha,iterat)
+    theta = np.copy(temp[0])
+    J = []
+    J = c.deepcopy(temp[1])
+    del temp[:]
+
 
 main()
