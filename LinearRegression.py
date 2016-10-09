5e6ef894d324126df4b5e7e891641b3f8dfd88c8
sergiores24 <sergiores24@gmail.com>
2016-10-09 14:54:30 -0500

    Media y desviasión estandar

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,16 +1,19 @@
 import numpy as np
 import random
-
 def featureNormalize(X):
 
     [m,n] = X.shape
     i=0
     j=0
-    media =[]
+    mean =[]
     std = []
-
-
-
+    for j in range(n):
+        mean.append(0.0)
+        for i in range(m):
+            mean[j]+=X[i,j]
+        mean[j]/=m
+        std.append(np.amax(X[:,j])-np.amin(X[:,j]))
+        return X
 
 def main():
     f = open('r_wpbc.data', 'r')
@@ -28,10 +31,8 @@ def main():
 
 
     y = []                      #Vector columna con salidas (Time)
-    i=0
-    for fila in x:
+    for i in range(194):
         y.append(x[i,32])
-        i += 1
 
     x = np.delete(x, 32, 1)     #Matriz x (entradas) es 194*32  (vector y eliminado de x)
 
@@ -46,7 +47,6 @@ def main():
 
     tests_ds = x[t2,:]          #Matriz con datos de prueba (40% de los datos iniciales)
 
-
-    featureNormalize(training_ds) #Normalizar datos del conjunto de entrenamiento
+    training_ds=featureNormalize(training_ds) #Normalizar datos del conjunto de entrenamiento
 
 main()
