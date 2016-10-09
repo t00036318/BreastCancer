2352e5a40c618e043fcb8f787efaec9eb602a0d3
Vanessa Esther Pacheco De La Rosa <t00036318@utbvirtual.edu.co>
2016-10-09 11:26:29 -0500

    Float

--- LinearRegression.py
+++ LinearRegression.py
@@ -2,15 +2,16 @@ import numpy as np
 
 f = open('r_wpbc.data', 'r')
 
+np.set_printoptions(suppress=True)
+
 data = []
 for line in f.readlines():
     line = line.strip()
-    line = line.split(",'")
+    line = line.split(",")
+    line = [float(i) for i in line]
     data.append(line)
-
 
 x = np.matrix(data)    #Matriz x (entradas) es 194*33
 
 print(x)
-
 
