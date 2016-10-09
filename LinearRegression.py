6c117b3889b767b81a2cf9b4dbdae65f6fe62d50
Vanessa Esther Pacheco De La Rosa <t00036318@utbvirtual.edu.co>
2016-10-09 12:23:07 -0500

    40%

--- LinearRegression.py
+++ LinearRegression.py
@@ -24,13 +24,13 @@ x = np.delete(x, 32, 1)     # Matriz x (entradas) es 194*32  (vector y eliminado
 
 
 t = random.sample(range(194),116)
-training_ds = x[t,:]        #Matriz con datos de  entrenamiento (60% de los datos iniciales)
+training_ds = x[t,:]        #Matriz con datos de entrenamiento (60% de los datos iniciales)
 
 t2 = []
 for element in range(194):
     if element not in t:
         t2.append(element)
 
-tests_ds = x[t2,:]          #Matriz con datos de  prueba (40% de los datos iniciales)
+tests_ds = x[t2,:]          #Matriz con datos de prueba (40% de los datos iniciales)
 
 
