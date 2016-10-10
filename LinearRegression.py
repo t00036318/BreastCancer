8c32ea04b6d151a22229d800dc8255100f52b7b2
Nicolas Agudelo <nicoalbert95@hotmail.com>
2016-10-10 18:26:57 -0500

    Adaptación
    Adaptación a nuevos conjuntos de datos

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,6 +1,6 @@
 import numpy as np
 import random
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 def featureNormalize(X):
 
     [m,n] = X.shape
@@ -42,7 +42,7 @@ def Pseudoinverse(X,Y):                            #Función que calcula la pseu
 
 def main():
 
-    f = open('r_wpbc.data', 'r')
+    f = open('ENB2012.csv', 'r')
 
     np.set_printoptions(suppress=True)
 
@@ -53,43 +53,55 @@ def main():
         line = [float(i) for i in line]
         data.append(line)
 
-    x = np.matrix(data)         #Matriz x (entradas) es 194*33
+    x = np.matrix(data)         #Matriz x (entradas) es 768*10
 
-
-    t = random.sample(range(194),116)
+    [m,n] = x.shape
+    t = random.sample(range(m),round((m*0.6)))
     training_ds = x[t,:]        #Matriz con datos de entrenamiento (60% de los datos iniciales)
 
     t2 = []
-    for element in range(194):
+    for element in range(m):
         if element not in t:
             t2.append(element)
 
     tests_ds = x[t2,:]          #Matriz con datos de prueba (40% de los datos iniciales)
-    y_training=training_ds[:,32]
-    y_test= tests_ds[:,32]
-    x = np.delete(x,32,1)       #Matriz x (entradas) es 194*32  (vector y eliminado de x)
-    training_ds = np.delete(training_ds,32,1)
-    tests_ds = np.delete(tests_ds,32,1)
+    y_training2=training_ds[:,n-1]
+    y_training=training_ds[:,n-2]
+    y_test2= tests_ds[:,n-1]
+    y_test = tests_ds[:,n-2]
+    x = np.delete(x,n-1,1)       #Matriz x (entradas) es 194*32  (vector y eliminado de x)
+    x = np.delete(x,n-2,1)
+    training_ds = np.delete(training_ds,n-1,1)
+    training_ds = np.delete(training_ds,n-2,1)
+    tests_ds = np.delete(tests_ds,n-1,1)
+    tests_ds = np.delete(tests_ds,n-2,1)
     [training_ds,mean,sigma]=featureNormalize(training_ds) #Normalizar datos del conjunto de entrenamiento
-    print (tests_ds)
     tests_ds = testNormalize(tests_ds,mean,sigma)
-    training_ds=np.concatenate((np.ones((116,1)),training_ds),axis=1)
-    tests_ds = np.concatenate((np.ones((78,1)),tests_ds),axis=1)
-    theta = np.zeros((33,1))
+    training_ds=np.concatenate((np.ones((round((m*0.6)),1)),training_ds),axis=1)
+    tests_ds = np.concatenate((np.ones(((m-round((m*0.6))),1)),tests_ds),axis=1)
+    [m,n] = x.shape
+    thetaG_y1 = np.zeros((n+1,1))
+    thetaN_y1 = np.zeros((n+1,1))
+    thetaG_y2 = np.zeros((n+1,1))
+    thetaN_y2 = np.zeros((n+1,1))
     alpha = 0.09
     iterat = 400
-    [theta,J] = gradientDescent(training_ds,y_training,theta,alpha,iterat)
-    theta2=Pseudoinverse(training_ds,y_training)
-    #print ("Thetas calculados por el metodo de Gradiente Descendiente: \n", theta)
-    print (tests_ds[1])
-    print (tests_ds*theta)
-    #print ("Thetas calculados por el metodo de la Pseudoinversa: \n",theta2)
-    print("holi")
-    print (tests_ds*theta2)
-    """plt.plot(J)
+    [thetaG_y1,J] = gradientDescent(training_ds,y_training,thetaG_y1,alpha,iterat)
+    [thetaG_y2,J2] = gradientDescent(training_ds,y_training2,thetaG_y2,alpha,iterat)
+    thetaN_y1 = Pseudoinverse(training_ds,y_training)
+    thetaN_y2 = Pseudoinverse(training_ds,y_training2)
+    print ("Thetas calculados por el metodo de Gradiente Descendiente para Heating load: \n", thetaG_y1)
+    print ("Thetas calculados por el metodo de Gradiente Descendiente para Cooling load: \n", thetaG_y2)
+    print ("Thetas calculados por el metodo de la Pseudoinversa para Heating load: \n",thetaN_y1)
+    print ("Thetas calculados por el metodo de la Pseudoinversa para Cooling load: \n",thetaN_y2)
+    plt.plot(J)
     plt.ylabel('Cost J')
     plt.xlabel('Number of Iterations')
-    plt.show()"""
+    plt.show()
+    plt.plot(J2)
+    plt.ylabel('Cost J')
+    plt.xlabel('Number of Iterations')
+    plt.show()
 
 
 main()
